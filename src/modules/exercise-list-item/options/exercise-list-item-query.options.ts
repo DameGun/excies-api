07a4de7c42cd2fb51578fdc39968c9c709064f80
Ilya Prymak <primak2506@gmail.com>
2024-12-13 16:13:19 +0300

    fix: exerciseListItem id dependency

--- src/modules/exercise-list-item/options/exercise-list-item-query.options.ts
+++ src/modules/exercise-list-item/options/exercise-list-item-query.options.ts
@@ -16,6 +16,7 @@ export const exerciseListItemQueryOptions: FindOneOptions<ExerciseListItem> = {
         name: true,
       },
     },
+    exerciseId: true,
     customName: true,
     lastTimeUpdated: true,
   },
